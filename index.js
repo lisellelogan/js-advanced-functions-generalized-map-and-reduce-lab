6fc8f5eedf07e6a03f3911e1bf422a40a5c26bde
lisellelogan <liselle.logan@yahoo.com>
2021-04-13 20:46:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+// MAP LIKE METHODS
+
+function map(arr, func){
+    let newArr = []
+
+    for (let i = 0; i < arr.length; i++){
+        let num = arr[i]
+        newArr.push(func(num))
+    }
+    return newArr;
+}
+
+// REDUCE LIKE METHODS
+function reduce(arr, func, startingPoint){
+    let newArr = (!!startingPoint) ? startingPoint : arr[0]
+    let i = (!!startingPoint) ? 0 : 1
+
+    for (; i < arr.length; i++){
+        newArr = func(arr[i], newArr)
+    }
+    return newArr;
+}
